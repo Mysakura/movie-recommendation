af6cb9a8805d7746cab6c168fb807440740e8d00
zgq7 <1140661106@qq.com>
2019-07-16 17:54:13 +0800

    test zgq7

--- movie-admin/src/main/java/com/dl/movieadmin/MovieAdminApplication.java
+++ movie-admin/src/main/java/com/dl/movieadmin/MovieAdminApplication.java
@@ -3,11 +3,14 @@ package com.dl.movieadmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 董盺杰 小吊吊
+ **/
 @SpringBootApplication
 public class MovieAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieAdminApplication.class, args);
+    }
 
 }
