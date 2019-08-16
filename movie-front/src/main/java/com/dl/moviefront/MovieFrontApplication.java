5ec8b5f674e5e70e8cddda0c8bb2c75f905d37a8
dongxinjie <dongxinjie@fcbox.com>
2019-08-16 18:13:59 +0800

    update

--- movie-front/src/main/java/com/dl/moviefront/MovieFrontApplication.java
+++ movie-front/src/main/java/com/dl/moviefront/MovieFrontApplication.java
@@ -2,8 +2,9 @@ package com.dl.moviefront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MovieFrontApplication {
 
 	public static void main(String[] args) {
